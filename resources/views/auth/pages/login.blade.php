f059e4c40d5c0719544f42a2b5602b65286ccc8c
DJ-Programmer-Micho <michelshabo1@gmail.com>
2024-07-26 17:15:46 +0300

    auth update

--- resources/views/auth/pages/login.blade.php
+++ resources/views/auth/pages/login.blade.php
@@ -1,14 +1,12 @@
 @extends('auth.layout')
 @section('content')
 <div class="container">
-    <!-- Outer Row -->
     <div class="row justify-content-center">
         <div class="col-xl-10 col-lg-12 col-md-9">
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
-                    {{-- @livewire('auth.login-livewire') --}}
-                    <livewire:auth.login-livewire />
+                    @livewire('auth.login-livewire')
                 </div>
             </div>
         </div>
