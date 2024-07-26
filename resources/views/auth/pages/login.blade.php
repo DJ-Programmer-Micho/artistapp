121268341e11dbc70981a268495a729ba5623581
DJ-Programmer-Micho <michelshabo1@gmail.com>
2024-07-26 17:13:15 +0300

    log livewire update

--- resources/views/auth/pages/login.blade.php
+++ resources/views/auth/pages/login.blade.php
@@ -7,7 +7,8 @@
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
-                    @livewire('auth.login-livewire')
+                    {{-- @livewire('auth.login-livewire') --}}
+                    <livewire:auth.login-livewire />
                 </div>
             </div>
         </div>
