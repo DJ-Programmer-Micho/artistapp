7b248cf20d6361e42b2672b63ed9223d0a8af037
DJ-Programmer-Micho <michelshabo1@gmail.com>
2024-07-27 16:37:47 +0300

    minor fix

--- resources/views/admins/pages/dashboard.blade.php
+++ resources/views/admins/pages/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="m-4">
     @livewire('admin.dashboard-card-livewire')
-    @livewire('admin.dashboard-artist-chart-livewire')
+    {{-- @livewire('admin.dashboard-artist-chart-livewire') --}}
     @livewire('admin.dashboard-table-livewire')
 </div>
 @endsection
